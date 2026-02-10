b508807a926b064e56549a713fc3c481da06cf60
pi0lin <adri090406@gmail.com>
2026-02-10 09:38:28 +0100

    spring project created

---
+++ olympo-academy/src/main/java/grupo10/olympo_academy/OlympoAcademyApplication.java
@@ -0,0 +1,13 @@
+package grupo10.olympo_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympoAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympoAcademyApplication.class, args);
+	}
+
+}
---
+++ olympo-academy/src/test/java/grupo10/olympo_academy/OlympoAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo10.olympo_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympoAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
