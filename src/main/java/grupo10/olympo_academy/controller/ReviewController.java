a7d43f5f570c4ab969c314651fb85c532cb0f961
pi0lin <adri090406@gmail.com>
2026-03-03 10:43:50 +0100

    Review Controller and Service nearly done

---
+++ src/main/java/grupo10/olympo_academy/controller/ReviewController.java
@@ -0,0 +1,48 @@
+package grupo10.olympo_academy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import grupo10.olympo_academy.model.Review;
+import grupo10.olympo_academy.services.ReviewService;
+
+@Controller
+@RequestMapping("/reviews")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    // Mostrar todas las reseñas
+    @GetMapping
+    public String listReviews(Model model) {
+        model.addAttribute("reviews", reviewService.getAllReviews());
+        model.addAttribute("review", new Review());
+        return "reviews"; // reviews.html
+    }
+
+    // Guardar reseña
+    @PostMapping("/save")
+    public String saveReview(@RequestParam int rating,
+                         @RequestParam String comment) {
+
+    Review review = new Review();
+    review.setRating(rating);
+    review.setComment(comment);
+    review.setDate(java.time.LocalDate.now().toString());
+
+    reviewService.saveReview(review);
+
+    return "redirect:/reviews";
+}
+
+    // Eliminar reseña
+    @GetMapping("/delete/{id}")
+    public String deleteReview(@PathVariable Long id) {
+        reviewService.deleteReview(id);
+        return "redirect:/reviews";
+    }
+}
+    
--- src/main/java/grupo10/olympo_academy/repositories/ReviewRepository.java
+++ src/main/java/grupo10/olympo_academy/repositories/ReviewRepository.java
@@ -1,8 +1,16 @@
 package grupo10.olympo_academy.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import grupo10.olympo_academy.model.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+     // Buscar reseñas por instalación
+    List<Review> findByFacilityId(Long facilityId);
+
+    // Buscar reseñas por usuario
+    List<Review> findByUserId(Long userId);
 }
+
