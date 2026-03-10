7aa3ac4abeb8b892ac26e15201a4815a955c6e12
Batcat248 <j.gonzaleza.2024@alumnos.urjc.es>
2026-03-10 09:37:42 +0100

    radirection if user tries to access my account without login

--- src/main/java/grupo10/olympo_academy/controller/UserController.java
+++ src/main/java/grupo10/olympo_academy/controller/UserController.java
@@ -1,5 +1,6 @@
 package grupo10.olympo_academy.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,31 +10,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserController {
 
+    
     @GetMapping("/login")
     public String getLogin() {
         return "login";
     }
+
     
-    
-    @PostMapping("/login") // <-- processes the form submission
+    @PostMapping("/login")
     public String postLogin(
             @RequestParam String user,
             @RequestParam String password,
-            Model model) {
+            Model model,
+            HttpSession session) {
 
-        //validates credentials
+        
         boolean ok = "demo@dominio.com".equals(user) && "1234".equals(password);
 
         if (ok) {
+            //we keep user session
+            session.setAttribute("usuarioLogeado", user);
             return "redirect:/userProfile";
         } else {
             model.addAttribute("loginError", "Usuario o contraseña incorrectos");
             return "login";
         }
     }
+
     
     @GetMapping("/userProfile")
-    public String getUserProfile() {
-        return "userProfile";
+    public String getUserProfile(HttpSession session) {
+
+        // if user is not login, move to login page
+        if (session.getAttribute("usuarioLogeado") == null) {
+            return "redirect:/login";
+        }
+
+        return "userProfile"; //protected page
+    }
+
+    // logout
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate(); 
+        return "redirect:/login";
     }
 }
