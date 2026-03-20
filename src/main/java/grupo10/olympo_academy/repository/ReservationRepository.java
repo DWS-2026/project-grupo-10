c5e66bc74af0726c33c6d14386544bb366a23286
Luxx67 <l.zapata.2024@alumnos.urjc.es>
2026-03-20 12:11:51 +0100

    new methods

--- src/main/java/grupo10/olympo_academy/repository/ReservationRepository.java
+++ src/main/java/grupo10/olympo_academy/repository/ReservationRepository.java
@@ -2,11 +2,13 @@ package grupo10.olympo_academy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import grupo10.olympo_academy.model.Classes;
 import grupo10.olympo_academy.model.Facility;
 import grupo10.olympo_academy.model.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 boolean existsByFacilityAndStatus(Facility facility, String status);
+boolean existsByClassesAndStatus(Classes classes, String status);
 
 
 }
