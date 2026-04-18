15c922e79e6b636489b35773ce9f0153d1ea06e6
Batcat248 <j.gonzaleza.2024@alumnos.urjc.es>
2026-04-18 10:09:20 +0200

    Added dependencies to pom

--- src/main/java/grupo10/olympo_academy/controllers/rest/UserRestController.java
+++ src/main/java/grupo10/olympo_academy/controllers/rest/UserRestController.java
@@ -1,5 +1,34 @@
 package grupo10.olympo_academy.controllers.rest;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import grupo10.olympo_academy.model.User;
+import grupo10.olympo_academy.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserRestController {
-    
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<User> me(HttpServletRequest request) {
+
+        Principal principal = request.getUserPrincipal();
+
+        if (principal != null) {
+            return ResponseEntity.ok(userService.findByEmail(principal.getName()));
+            //userRepository.findByName(principal.getName()).orElseThrow()
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
