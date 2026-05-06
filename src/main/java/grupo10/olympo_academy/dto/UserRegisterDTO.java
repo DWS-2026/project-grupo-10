dc6cbef272dea3528453a0ab2dcfd263e2c4787e
Batcat248 <j.gonzaleza.2024@alumnos.urjc.es>
2026-05-07 00:33:08 +0200

    Fixed user registration bug: users could be register without completing all required parameters

--- src/main/java/grupo10/olympo_academy/dto/UserRegisterDTO.java
+++ src/main/java/grupo10/olympo_academy/dto/UserRegisterDTO.java
@@ -1,10 +1,16 @@
 package grupo10.olympo_academy.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record UserRegisterDTO (
+    @NotBlank(message = "name is required")
     String name,
+    @NotBlank(message = "email is required")
     String email,
     String phone,
+    @NotBlank(message = "username is required")
     String username,
+    @NotBlank(message = "password is required")
     String password) 
     {
 }
