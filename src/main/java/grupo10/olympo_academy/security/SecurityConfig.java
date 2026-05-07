760e0ee054200be33ba0cedc3610c21ebbc4eb7a
pi0lin <adri090406@gmail.com>
2026-05-08 00:45:02 +0200

    last imports; end of project

--- src/main/java/grupo10/olympo_academy/security/SecurityConfig.java
+++ src/main/java/grupo10/olympo_academy/security/SecurityConfig.java
@@ -60,16 +60,23 @@ public class SecurityConfig {
         http.headers(headers -> headers
                 .contentSecurityPolicy(csp -> csp
                         .policyDirectives(
-                                "default-src 'self'; " +
-                                        "script-src 'self' https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js https://use.fontawesome.com/releases/v6.3.0/js/all.js; https://cdn.quilljs.com"
-                                        +
-                                        "object-src 'none'; " +
-                                        "base-uri 'self'; " +
-                                        "frame-ancestors 'self'; " +
-                                        "img-src 'self' data:; " +
-                                        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.quilljs.com"
-                                        +
-                                        "font-src 'self' https://fonts.gstatic.com")));
+                        "default-src 'self'; " +
+                        "script-src 'self' " +
+                                "https://cdn.jsdelivr.net " +
+                                "https://use.fontawesome.com " +
+                                "https://cdn.quilljs.com; " +
+                        "style-src 'self' 'unsafe-inline' " +
+                                "https://cdn.jsdelivr.net " +
+                                "https://fonts.googleapis.com " +
+                                "https://cdn.quilljs.com; " +
+                        "font-src 'self' https://fonts.gstatic.com; " +
+                        "img-src 'self' data:; " +
+                        "object-src 'none'; " +
+                        "base-uri 'self'; " +
+                        "frame-ancestors 'self';"
+                        )
+                )
+                );
 
         http
                 .authorizeHttpRequests(authorize -> authorize
